217df397401f5bea1945c3fed660c1593bd2352f
Tim Hachigian <tim@hiddenmountaindata.com>
2024-04-15 14:48:41 -0600

    Update README with new plugin configurations

---
+++ lua/custom/plugins/catppuccin.lua
@@ -0,0 +1,32 @@
+return {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+    config = function()
+      require('catppuccin').setup {
+        flavour = 'macchiato',
+        integrations = {
+          gitsigns = true,
+          mason = true,
+          mini = {
+            enabled = true,
+            indentscope_color = '',
+          },
+          neotree = true,
+          treesitter = true,
+          telescope = {
+            enabled = true,
+          },
+        },
+        -- transparent_background = false,
+        -- color_overrides = {
+        --   macchiato = {
+        --     base = '#23263A',
+        --   },
+        -- },
+      }
+    end,
+    init = function()
+      vim.cmd.colorscheme 'catppuccin'
+    end,
+  }
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,20 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+    'github/copilot.vim',
+    config = function()
+      vim.g.copilot_assume_mapped = true
+      vim.keymap.del('i', '<Tab>')
+  
+      vim.keymap.set('i', '<M-\\>', '<Plug>(copilot-suggest)', { silent = true })
+  
+      vim.keymap.set('i', '<C-J>', '<Nop>', { noremap = true })
+      vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
+        expr = true,
+        replace_keycodes = false,
+      })
+      vim.g.copilot_no_tab_map = true
+      vim.g.copilot_filetypes = { ['*'] = false }
+    end,
+  }
